158d06e6252f66ca0d8b31517f5cd1159bcfa187
Juanmihd <juanmihd@gmail.com>
2015-11-01 11:54:59 +0000

    Initial commit. Just adding a hello world and the Visual Studio project

---
+++ TBSMaker/main.cpp
@@ -0,0 +1,12 @@
+// @author Juanmi.Huertas
+// 
+// This is the main file for the project TBSMaker.
+// The initial version is just a test-sample of basic funcionality
+// Main objective: create data structures for the game and resolve a turn of the game.
+
+#include "stdio.h"
+
+int main(int argc, char** argv)
+{
+  printf("TBSMaker is here!");
+}
