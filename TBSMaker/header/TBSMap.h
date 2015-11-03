f3c91e61edeba488cec93c375d23c3fafc4988af
Juanmihd <juanmihd@gmail.com>
2015-11-03 18:53:25 +0000

    Adding some comments, and get function for TBSLocations

--- TBSMaker/header/TBSMap.h
+++ TBSMaker/header/TBSMap.h
@@ -7,9 +7,9 @@
 
 //TEMPORARY
 #include <string> //I want to use "atoms" instead of strings
-#include <unordered_map> // I want to use sparse_hash instead of unordered_map
+#include <unordered_map> // I want to use sparse_hash instead of unordered_map  
 
-// @brief Contains all the info for a location of the game
+// @brief Contains all the info for a location of the game (all this info will be const, and will not change throughout the game
 class TBSLocation{
   enum TBSLocationType{ _UNPASSABLE, _WATER, _GROUND };
   // Complete name of the location
@@ -21,34 +21,32 @@ class TBSLocation{
   // TEMPORARY using string until I have atoms into functioning
   std::string short_name_;
 
-  // List of connected locations
-  std::vector< std::weak_ptr< TBSLocation > > links_;
+  // Hash table of connected locations
+  std::unordered_map < std::string, std::weak_ptr< TBSLocation > > links_;
 
   // Type of the location
   TBSLocationType type_;
-
-  // Index of player controling location
-  unsigned char owner_;
-
-  
-
 };
 
 // @brief Contains all the info for a map of the game
 class TBSMap{
   // Array with all the locations, to keep them packed together.
-  std::vector < std::shared_ptr< TBSLocation > > array_locations_;
+  std::vector < std::shared_ptr< TBSLocation > > locations_;
 
   // Hash table with pointers to each of the locations (by name), to keep an inmediate access to them.
   // here I will use a better container than those offered by the stl
   // TEMPORARY using unordered_map (the best that the stl can offer)
-  std::unordered_map< std::string, std::weak_ptr< TBSLocation > > hash_locations_;
+  std::unordered_map< std::string, int > location_ids_;  //This could be simplified by having an atom structure for Locations (matching a STRING with an ID)
 
   // Image of the map
   // here I will have probably the reference to the image 
   // TEMPORARY still using text based version, so no image needed
   // image variable
 
-
+  // @brief This functions get a pointer to a location from the short name
+  // @param location String with the abreviation of the location
+  std::weak_ptr < TBSLocation > get_location(std::string location_name){
+    return location_ids_.count(location_name) <= 0 ? nullptr : locations_[location_ids_[location_name]];
+  }
 
 };
